251254e4afd6725ca5879f731d1c04f73ef0934f
LiHongGai <321142131@qq.com>
2020-01-10 10:10:15 +0800

    11

---
+++ DataStructuresAndAlgorithm/AverageAge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AverageAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
